d5baa4171b476bc528e11327b3d0086afe0d206b
Wilson <nicolas.echeverry@correounivalle.edu.co>
2022-08-31 16:01:56 -0500

    Terminada la aplicación de Widgets. Seguir con la app de películas de la sección 7 (video 107)

--- lib/screens/list_view_builder_screen.dart
+++ lib/screens/list_view_builder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/themes/app_theme.dart';
 
 // Para realizar un scroll infnito, se requiere que la clase sea extendida del StatefulWidget
 
@@ -10,7 +11,6 @@ class ListViewBuilderScreen extends StatefulWidget {
 }
 
 class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
-  
   final List<int> imagesIds = [
     1,
     2,
@@ -23,6 +23,8 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     9,
     10
   ]; // Arreglo para pedir las IDs de las imágenes
+
+  bool isLoading = false;
 
   //* Cómo hago para saber que estoy llegando a las 10 imágenes?
   //* R/. Por lo general, los widets que permiten hacer scroll tienen un controlador
@@ -48,9 +50,41 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
       if ((scrollController.position.pixels + 500) >=
           scrollController.position.maxScrollExtent) {
-        add5();
+        //add5();
+        fetchData(); // realiza la petición
       }
     });
+  }
+
+  //* En lugar de realizar la carga rápidamente, en la realidad existe un tiempo de espera entre la carga de las imágenes y la visualización de la App. Aquí se logra usando un Future
+
+  Future fetchData() async {
+    // Esto simula el tiempo de petición para llamar las variables
+
+    if (isLoading) return; // Para que no esté todo el rato haciendo perticiones
+
+    isLoading = true; // inicia la petición
+
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3)); // espera de 3 segundos
+
+    add5(); // Llama a la función que carga imágenes adicionales
+
+    isLoading = false; // termina la petición
+    setState(() {});
+
+    //* Si no estoy cerca del final, no hagas la animación
+
+    if (scrollController.position.pixels + 100 <=
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(
+        //* Animación para indicarle al usuario que siga haciendo scroll
+        scrollController.position.pixels +
+            120, //? A qué punto del nuevo scroll necesito moverme?
+        duration: const Duration(milliseconds: 300), // Duración
+        curve: Curves.fastOutSlowIn); // Tipo de animación
   }
 
   // Función para añadir elementos
@@ -63,10 +97,23 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     setState(() {});
   }
 
+  Future<void> onRefresh() async {
+    // Future para realizar el refresh al hacer pull
+
+    final lastID = imagesIds.last;
+    await Future.delayed(const Duration(seconds: 2)); // espera de 2 segundos
+    imagesIds.clear();
+    imagesIds.add(lastID + 1);
+    add5();
+  }
+
   // 2. La creación del Statefulwidget como tal (creo, la verdad no lo mencionan)
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context)
+        .size; // determinar el tamaño del dispositivo donde está corriendo mi app
+
     return Scaffold(
       backgroundColor: Colors.black, // Color de fondo del Scaffold
 
@@ -77,28 +124,73 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
         removeTop: true, // Quita el padding superior
         removeBottom: true, // Quita el padding inferior
 
-        child: ListView.builder(
-            controller: scrollController,
-            physics:
-                const BouncingScrollPhysics(), // Para dar el efecto de rebote como en IOS al estar en Android
-            itemCount: imagesIds.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Column(
-                children: [
-                  // const SizedBox(
-                  //   height: 20,
-                  // ),
-                  FadeInImage(
-                      width: double.infinity, // tomar todo el ancho posible
-                      height: 300,
-                      fit: BoxFit.cover, // Tomar todo el espacio
-                      placeholder: const AssetImage('assets/jar-loading.gif'),
-                      image: NetworkImage(
-                          'https://picsum.photos/500/300?image=${imagesIds[index]}') // PicSum es una API de imágenes aleatorias
-                      ),
-                ],
-              );
-            }),
+        child: Stack(
+          // Widget que apila Widgets xd
+          children: [
+            RefreshIndicator(
+              // Permite recargar las imágenes al hacer pull (tirar hacia arriba)
+              
+              color: AppTheme.primary,
+              
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                  controller: scrollController,
+                  physics:
+                      const BouncingScrollPhysics(), // Para dar el efecto de rebote como en IOS al estar en Android
+                  itemCount: imagesIds.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: [
+                        // const SizedBox(
+                        //   height: 20,
+                        // ),
+                        FadeInImage(
+                            width:
+                                double.infinity, // tomar todo el ancho posible
+                            height: 300,
+                            fit: BoxFit.cover, // Tomar todo el espacio
+                            placeholder:
+                                const AssetImage('assets/jar-loading.gif'),
+                            image: NetworkImage(
+                                'https://picsum.photos/500/300?image=${imagesIds[index]}') // PicSum es una API de imágenes aleatorias
+                            ),
+                      ],
+                    );
+                  }),
+            ),
+            if (isLoading) // Si está cargardo, muestra el loading. //?Nótese que el if no acepta {}, por lo tanto, solo se puede una instrucción. También soporta -else-
+
+              Positioned(
+                  //? Este es un Widget útil para posicionar otros Widgets dentro de una
+                  bottom: 40,
+                  left: (size.width / 2) -
+                      30, //? Se toma la mitad del ancho del dispositivo y se le resta la mitad de las dimensiones del container para el ícono de carga
+                  child: const _Loadingicon()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Loadingicon extends StatelessWidget {
+  const _Loadingicon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      // color: Colors.white, //*El color no puede junto al decoration, debe ir DENTRO del decoration
+      height: 60,
+      width: 60,
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.7),
+          shape: BoxShape.circle //* También funciona la propiedad borderRadius
+          ),
+      child: const CircularProgressIndicator(
+        color: AppTheme.primary,
       ),
     );
   }
